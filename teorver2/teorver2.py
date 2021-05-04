8967c5000b877e7119b720e9e2f8832ae123c18f
ZaKSa <zacksenia@yandex.ru>
2021-05-04 18:50:04 +0400

    04/05

--- teorver2/teorver2.py
+++ teorver2/teorver2.py
@@ -38,7 +38,7 @@ for i in range(n):
     xi[i] = double(input.readline())
 input.close()
 
-#количество интервалов 4
+#количество интервалов 10
 N = floor(1+ 3.32*log(n, 10)) + 1
 print("N> "+str(N)+"\n")
 #Подсчитываем частоты Vk попадания выборочных значений в k-ый интервал kJ
@@ -56,10 +56,20 @@ for i in range(N):
             vk[i]+=1
 text.write("chastota> "+str(vk)+"\n")
 
+sum0=0
+for i in range(N):
+    sum0+=vk[i]
+text.write("summ> "+str(sum0) +"\n")
+
 pk=[0]*N
 for i in range(N):
     pk[i]=vk[i]/n
 text.write("otnosit chastota> "+str(pk) +"\n")
+
+sum=0
+for i in range(N):
+    sum+=pk[i]
+text.write("summ> "+str(sum) +"\n")
 
 hk=[0]*N
 for i in range(N):
@@ -74,8 +84,6 @@ for i in range (N):
 text.write("teor znachenie f> "+str(f) + "\n")
 
 def func(xi):
-    #global xi
-    global n
     global a
     global k
     global count
@@ -84,11 +92,11 @@ def func(xi):
         f[i] = (a*(a*xi[i])**(k-1)*e**(-a*xi[i]))/(math.factorial(k-1))
     return f
 
-x = np.linspace(min(xi), max(xi), count)
+#x = np.linspace(min(xi), max(xi), count)
 
-plt.hist(xi, bins = N, density=True)
-plt.plot(x, func(x))
-plt.show()
+#plt.hist(xi, bins = N, density=True)
+#plt.plot(x, func(x))
+#plt.show()
 
 #2.2
 mx1 = 0
@@ -103,8 +111,38 @@ for i in range(n):#выборочная дисперсия
     dx1+=(xi[i]-mx1)**2
 dx1/=n
 text.write("dx1= " +str(dx1)+"\n")
-#2.3
 
+
+####
+dx_no = 0
+for i in range(n):#выборочная дисперсия несмещенная
+    dx_no+=(xi[i]-mx1)**2
+dx_no/=(n-1)
+text.write("dx1 nesmeshennaya= " +str(dx_no)+"\n")
+
+
+x2 = 0
+for i in range(N):#
+    mid = ((min(xi)+u*i)+min(xi)+u*(i+1))/2
+    x2 += vk[i]*mid
+x2/=n
+text.write("mx2= " +str(x2)+"\n")
+
+
+dx2 = 0
+for i in range(N):#
+    mid = ((min(xi)+u*i)+min(xi)+u*(i+1))/2
+    dx2+=(mid-x2)**2*vk[i]
+dx2/=n
+text.write("dx2= " + str(dx2)+"\n")
+
+
+#2.3 метод моментов
+k_m=(mx1)**2/dx1
+text.write("metod momentov> k= "+ str(k_m)+"\n")
+
+a_m=mx1/dx1
+text.write("metod momentov> a= "+ str(a_m)+"\n")
 #2.4 Построить довер интервалы
 
 #(1+y)/2=0.95
@@ -131,3 +169,99 @@ text.write("приближенный дов для Д\n"+"t1_7= "+str(t1_7)+"\n"
 text.write("t2_7= "+str(t2_7)+"\n")
 
 #2.5
+
+#1.5
+#привести значения статистики критерия и порога
+#порог
+#1-alfa=0.975, (N-1)-1=8
+hi2=17.535
+#1-alfa=0.975, (N-1)-1-r=9-1-2=6
+hi2_un=14.449
+#объединеним последний и предпоследний интервалы, первй и второй
+N_=N-1
+p_un = [0]*(N_)
+
+interv_=[0]*(N_+1)
+text.write("posle > ")
+interv_[0]=min(xi)
+text.write(str(min(xi))+"\n")
+for i in range (1,N_):
+    interv_[i]=min(xi)+u*(i)
+    text.write(str(min(xi)+u*(i))+"\n")
+interv_[N_]=max(xi)
+text.write(str(max(xi))+"\n"+"\n")
+for i in range (N_+1):
+    text.write(str(interv_[i])+"\n")
+text.write("\n")
+
+vk_ = [0]*N_
+text.write("posle chastota> ")
+for i in range(N_-1):
+    vk_[i]=vk[i]
+vk_[-1]=vk[-1]+vk[-2]
+for i in range(N_):
+    text.write(str(vk_[i])+"\n")
+
+
+pk_=[0]*N_
+text.write("posle otnosit chastota> ")
+for i in range(N_-1):
+    pk_[i]=pk[i]
+pk_[-1]=pk[-1]+pk[-2]
+for i in range(N_):
+    text.write(str(pk_[i]) +"\n")
+
+sum1=0
+for i in range(N_):
+    sum1+=vk_[i]
+text.write("summ> "+str(sum1) +"\n")
+
+sum2=0
+for i in range(N_):
+    sum2+=pk_[i]
+text.write("summ> "+str(sum2) +"\n")
+
+text.write("posle teor znachenie f> ")
+text.write(str(norm.pdf(((min(xi))+min(xi)+u*2)/2, loc = a, scale= gamma)) + "\n")
+for i in range (1, N_-1):
+    mid_ = ((min(xi)+u*(i+1))+min(xi)+u*(i+2))/2
+    #f_ = math.exp((mid_-a)**2/(2*gamma2))/(gamma*sqrt(2*math.pi))
+    text.write(str(norm.pdf(mid_, loc = a, scale= gamma)) + "\n")
+text.write(str(norm.pdf(min(xi)+u*9, loc = a, scale= gamma)) + "\n")
+
+######################################################################
+#статистика критерия
+#(a*(a*u_)**(k-1)*
+def F_un(u_):
+    F_u=0
+    global k
+    global a
+    for i in range(k-1):
+        F_u+=(a*u_)**i/(math.factorial(i))
+    F_u=1-(e**(-a*u_)*F_u)
+    
+    return F_u#norm.cdf(u_, loc = mx1, scale = sqrt(dx1))
+
+#p = [0]*(N_)
+hi_stat=0
+hi_stat_unknown=0
+
+
+for i in range(N_-1):#######################
+     p_un[i]=F_un(min(xi)+u*(i+1))-F_un(min(xi)+u*(i))
+     #hi_stat_unknown += (vk_[k]-n*p_un[k])**2/(n*p_un[k])
+p_un[N_-1]=F_un(min(xi)+u*10)-F_un(min(xi)+u*8)
+
+for i in range(N_):##
+     hi_stat_unknown += (vk_[i]-n*p_un[i])**2/(n*p_un[i])
+
+
+text.write("veroyatnost' un "+(str(p_un))+ "\n")
+    
+text.write(str(hi_stat_unknown)+"\n")
+
+
+if hi_stat_unknown >= hi2_un:
+    text.write("гипотезу H0 отвергают\n")
+else:
+    text.write("гипотезу H0 принимают\n")
